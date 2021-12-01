204709101d27167f5443415503b49d388ee3fbcd
Matheus <mms.medeiros12@gmail.com>
2021-12-01 16:06:39 -0300

    feat: Class challenge

---
+++ classes/classes.ts
@@ -0,0 +1,64 @@
+// Exercício 1 - Classe
+class Motoo {
+  nome: string;
+  velocidade: number;
+
+  constructor(nome: string, velocidade: number = 0) {
+    this.nome = nome;
+    this.velocidade = velocidade;
+  }
+
+  buzinar(): void {
+    console.log('Toooooooooot!');
+  }
+
+  acelerar(delta: number): void {
+    this.velocidade = this.velocidade + delta;
+  }
+}
+
+var motoo = new Moto('Ducati');
+moto.buzinar();
+console.log(moto.velocidade);
+moto.acelerar(30);
+console.log(moto.velocidade);
+
+// Exercício 2 - Herança
+var objeto2D = {
+  base: 0,
+  altura: 0,
+};
+
+var retanguloo = Object.create(objeto2D);
+retangulo.base = 5;
+retangulo.altura = 7;
+retangulo.area = function () {
+  return this.base * this.altura;
+};
+console.log(retangulo.area());
+
+// Exercício 3 - Getters & Setters
+var estagiarioo = {
+  _primeiroNome: '',
+};
+
+Object.defineProperty(estagiario, 'primeiroNome', {
+  get: function () {
+    return this._primeiroNome;
+  },
+  set: function (valor) {
+    if (valor.length >= 3) {
+      this._primeiroNome = valor;
+    } else {
+      this._primeiroNome = '';
+    }
+  },
+  enumerable: true,
+  configurable: true,
+});
+
+console.log(estagiario.primeiroNome);
+estagiario.primeiroNome = 'Le';
+console.log(estagiario.primeiroNome);
+estagiario.primeiroNome = 'Leonardo';
+console.log(estagiario.primeiroNome);
